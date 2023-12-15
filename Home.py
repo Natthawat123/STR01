b7faa6240ea0e3f57a2c1aa894b0af5ca239c55b
Natthawat123 <kankiritothekidstudio@gmail.com>
2023-12-15 14:17:58 +0700

    .commit

--- Home.py
+++ Home.py
@@ -7,3 +7,11 @@ st.header('Natthawat Hiranwong')
 data_dict = {'PHP':33, 'C':28, 'Java':30}
 courses = list(data_dict.keys())
 values = list(data_dict.values())
+
+fig = plt.figure(figsize = (10, 5))
+# Bar plot
+plt.bar(courses, values, color ='green',width = 0.5)
+plt.xlabel("Courses ")
+plt.ylabel("No. of students enrolled")
+plt.title("Students enrolled in different courses")
+plt.show()
