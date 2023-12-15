e162204f51e75a247028bc7f51e36947fe28fc4b
Natthawat123 <kankiritothekidstudio@gmail.com>
2023-12-15 14:31:32 +0700

    .commit

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/john.jifi')
+    st.image('./pic/john.jfif')
 with col2:
     st.image('./pic/iris.jfif')
 
